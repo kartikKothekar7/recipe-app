afb2d22a8605ef6494af2d6dd0224f23dea2e5aa
Kartik Kothekar <kartikkothekar7@gmail.com>
2026-01-17 23:23:02 +0530

    Set up CORS for specific frontend origin
    Configure CORS middleware to allow requests from the specified origin.

--- FoodRecipe/backend/server.js
+++ FoodRecipe/backend/server.js
@@ -1,14 +1,20 @@
 const express=require('express');
+const cors=require('cors') //This line imports the CORS middleware so your backend can handle requests coming from a different origin.
 const app=express();
 const dotenv=require('dotenv').config();
 const connectDb=require("./config/connectionDb")  // we require connection functionality here and then call the connectDb() as server.js is running
-const cors=require('cors') //This line imports the CORS middleware so your backend can handle requests coming from a different origin.
 
 const PORT=process.env.PORT || 3000;
 connectDb(); 
 
+app.use(cors({
+    origin: [
+        "https://recipe-app-fronted.onrender.com"
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true
+}));
 app.use(express.json())
-app.use(cors())
 app.use(express.static("public"))
 
 app.use("/",require("./routes/user"))
